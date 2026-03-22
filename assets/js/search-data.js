52f0af01a456bd2494fbc8accc7289a32f9d5aeb
AlexGieselman <106560667+AlexGieselman@users.noreply.github.com>
2026-03-22 01:07:36 +0000

    Deploying to gh-pages from @ AlexGieselman/alexgieselman.github.io@07b2e4310f490c1db39e0fae3aa5a3779dc65f9b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "post-welcome-to-my-blog",
+        
+          title: "Welcome to My Blog",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/welcome/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
